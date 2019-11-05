9cd6b474aeee416fc1a9ed1ef0f79c74f5e6bfa5
unknown <buffinzz@gmail.com>
2019-11-04 19:17:36 -0500

    test connect google

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
+require("./credentials.js");
 
-let commands = require("./commands.js");
+let google = require('googleapis');
+let privatekey = require(process.env.GOOGLE_FILE);
 
 
 const client = new Discord.Client();
@@ -27,7 +29,21 @@ client.on('message', message => {
 });
 
  
-
+// configure a JWT auth client
+let jwtClient = new google.auth.JWT(
+    privatekey.client_email,
+    null,
+    privatekey.private_key,
+    ['https://www.googleapis.com/auth/calendar']);
+//authenticate request
+jwtClient.authorize(function (err, tokens) {
+if (err) {
+console.log(err);
+return;
+} else {
+console.log("Successfully connected!");
+}
+});
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
