513b352cacb781acf3bb602f451b098bbcc60d6e
unknown <buffinzz@gmail.com>
2019-11-04 19:52:40 -0500

    test google

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 //require("./credentials.js");
 
 const {google} = require('googleapis');
-
+const compute = google.compute('v1');
 
 
 const client = new Discord.Client();
@@ -28,14 +28,32 @@ client.on('message', message => {
 
 });
 
-const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URL
-  );
-google.options({
-    auth: oauth2Client
-});
+// const oauth2Client = new google.auth.OAuth2(
+//     process.env.GOOGLE_CLIENT_ID,
+//     process.env.GOOGLE_CLIENT_SECRET,
+//     process.env.GOOGLE_REDIRECT_URL
+//   );
+// google.options({
+//     auth: oauth2Client
+// });
+const compute = google.compute('v1');
+ 
+async function main () {
+  // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
+  // environment variables.
+  const auth = new google.auth.GoogleAuth({
+    // Scopes can be specified either as an array or as a single, space-delimited string.
+    scopes: ['https://www.googleapis.com/auth/compute']
+  });
+  const authClient = await auth.getClient();
+ 
+  // obtain the current project Id
+  const project = await auth.getProjectId();
+ 
+  // Fetch the list of GCE zones within a project.
+  const res = await compute.zones.list({ project, auth: authClient });
+  console.log(res.data);
+}
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
