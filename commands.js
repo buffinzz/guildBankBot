dd83c574150189296b7e31c51a0901fe27a3cd92
unknown <buffinzz@gmail.com>
2019-11-04 19:06:18 -0500

    err logging

--- commands.js
+++ commands.js
@@ -4,9 +4,11 @@ const path = process.env.GOOGLE_FILE;
 
 fs.access(path, fs.F_OK, (err) => {
   if (err) {
-    console.error(err)
-    fs.writeFile(process.env.GOOGLE_FILE, process.env.GOOGLE_CREDS, (err) => {});
-    console.error(err)
+    fs.writeFile(process.env.GOOGLE_FILE, process.env.GOOGLE_CREDS, (err) => {
+        console.error(err)
+    }
+    );
+    
   }
 
   //file exists
