2b1d18b591361aa69f238ce60143d7c5c4bbce27
unknown <buffinzz@gmail.com>
2019-11-04 19:18:48 -0500

    test google connect

---
+++ credentials.js
@@ -0,0 +1,16 @@
+const fs=require('fs');
+
+const path = process.env.GOOGLE_FILE;
+
+try {
+    if (fs.existsSync(path)) {
+      //file exists
+      console.log('exists!')
+    }
+  } catch(err) {
+    fs.writeFile(process.env.GOOGLE_FILE, process.env.GOOGLE_CREDS, (err) => {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+  }
+
